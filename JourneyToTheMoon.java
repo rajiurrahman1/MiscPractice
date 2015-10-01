009755bcf4f936af77f39f78585784e55400434a
Rajiur Rahman <rajiurrahman.bd@gmail.com>
2015-10-01 19:19:51 -0400

    Added problem statement from HackerRank.com

--- JourneyToTheMoon.java
+++ JourneyToTheMoon.java
@@ -5,6 +5,16 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
+ * Problem Statement: From Hacker Rank
+ * The member states of the UN are planning to send 2 people to the Moon. But there is a problem. In line with their principles of global unity, they want to pair astronauts of 2 different countries.
+ * There are N trained astronauts numbered from 0 to N-1. But those in charge of the mission did not receive information about the citizenship of each astronaut. 
+ * The only information they have is that some particular pairs of astronauts belong to the same country. 
+ * Your task is to compute in how many ways they can pick a pair of astronauts belonging to different countries. Assume that you are provided enough pairs to let you identify the groups of astronauts even though you might not know their country directly. 
+ * For instance, if 1,2,3 are astronauts from the same country; it is sufficient to mention that (1,2) and (2,3) are pairs of astronauts from the same country without providing information about a third pair (1,3). 
+ * 
+ * 
+ * Problem breakdown:
+ * The main thing to understand is to form a connected graph from the input. The is basic combination logic from standard 7. 
  *
  * @author Rajiur
  */
