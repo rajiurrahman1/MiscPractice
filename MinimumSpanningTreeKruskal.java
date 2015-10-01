21f328f7065b3af5994f0705de3d015ee97531a7
Rajiur Rahman <rajiurrahman.bd@gmail.com>
2015-09-30 20:40:51 -0400

    Implementing minimum spanning tree Krushkal's Algorithm

---
+++ MinimumSpanningTreeKruskal.java
@@ -0,0 +1,203 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rajiur
+ */
+
+class Edge{
+    public int src;
+    public int dst;
+    public Edge(int s, int d){
+        this.src = s;
+        this.dst = d;
+    }
+}
+
+public class MinimumSpanningTreeKruskal {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int numNodes = in.nextInt();
+        int numEdges = in.nextInt();
+        
+        //the graph will be saved as a HashMap where the key will be edge weight
+        //and the value of HashMap will be a list of type <Edge>. The list will contain all the edges with similar weights
+        //putting weight as Key for the convenience of implementing Kruskal algorithm. In each iteration, we need the smallest weight
+        HashMap<Integer, List<Edge>> graph = new HashMap<Integer, List<Edge>>();
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        
+        for(int i=0; i<numEdges; i++){
+            int source = in.nextInt();
+            int destination = in.nextInt();
+            int weight = in.nextInt();
+            addEdge(graph, source, destination, weight);
+            pq.add(weight);
+        }
+        int startNode = in.nextInt();
+//        printGraph(graph);
+        List<Edge> mstEdges = new ArrayList<Edge>();
+        int mstSum = mstKruskal(numNodes, graph, pq, mstEdges);
+//        System.out.println(pq.toString());
+        System.out.println(mstSum);
+
+    }
+    private static int mstKruskal(int numNodes, HashMap<Integer, List<Edge>> graph, PriorityQueue<Integer> pq, List<Edge> mstEdges ) {
+        //first we build a set to maintain if it is safe to add an edge to MST
+        //and write a method using this set // if adding an edge forms cycle, the method returns false
+        HashMap<Integer, List<Integer>> set = buildSet(numNodes); // build the set to check cycle before adding edge to MST
+        int mstWeight = 0;
+//        printSet(set);
+        
+//        int minWeight = pq.poll();
+//        Edge e = getEdge(graph, minWeight);
+//        System.out.println(e.src + "-" + e.dst);
+//        boolean res = isSafe(set, e.src, e.dst);
+//        System.out.println(res);
+//        printSet(set);
+
+        while( !pq.isEmpty() ){
+            int minWeight = pq.poll();
+            Edge e = getEdge(graph, minWeight);
+            boolean res = isSafe(set, e.src, e.dst);
+            if(res){ //if res is true, add them in mstWeight and edges
+                mstWeight += minWeight;
+                mstEdges.add(e);
+            }
+        }
+        return mstWeight;
+    }
+    
+    //the following method has two parts
+    //part 1 - to find out if src and dst are in a same set or not
+            //if yes, its not safe to add this edge into MST, therefore return false
+    //part 2- if not same set, then adjust the set so that src and dst both are in same set, the return true
+    private static boolean isSafe(HashMap<Integer, List<Integer>> set, int src, int dst) {
+        //find wehre 'src' is taking rest
+        int srcIndex = -1;
+        for(Integer key:set.keySet()){
+            List<Integer> list1 = set.get(key);
+            for(Integer i:list1){
+                if(i==src){
+                    srcIndex = key;
+                    break;
+                }
+            }
+        }
+        //now its the turn of dst
+        int dstIndex = -1;
+        for(Integer key:set.keySet()){
+            List<Integer> list1 = set.get(key);
+            for(Integer i:list1){
+                if(i==dst){
+                    dstIndex = key;
+                    break;
+                }
+            }
+        }
+        if(srcIndex == dstIndex){ //both the nodes are in same set, not safe to add the node.
+            return false;
+        }
+        else{
+            //the source and destination nodes are in different set
+            //therefore, its safe to add this edge into MST
+            //adjust the set before returning
+            List<Integer> srcList = set.get(srcIndex);
+            List<Integer> dstList = set.get(dstIndex);
+            for(Integer element:dstList){
+                srcList.add(element);
+            }
+            set.remove(dstIndex);  
+            set.put(srcIndex, srcList);
+            return true;
+        }
+    }
+    private static int finDstIndexInTheList(List<Integer> dstList, int dst) {
+        int x = 0;
+        for(int i=0; i<dstList.size(); i++){
+            if(dstList.get(i) == dst){
+                x = i;
+                break;
+            }
+        }
+        return x;
+    }
+    
+    private static Edge getEdge(HashMap<Integer, List<Edge>> graph, int minWeight) {
+        List<Edge> edgeList = graph.get(minWeight);
+        if(edgeList.size() == 1){ 
+            // there is only one edge associated with this weight
+            // get List from Hash map which contains only one edge, delete the HashMap entry for that weight and return edge
+            Edge e = edgeList.get(0);
+            graph.remove(minWeight);
+            return e;
+        }
+        else{ 
+            //there are multiple edges for this weight
+            //get the list, from the list get the first edge
+            //remove the first edge from the list, return the edge
+            Edge e = edgeList.get(0);
+            edgeList.remove(0);
+            graph.put(minWeight, edgeList);
+            return e;
+        }
+    }
+    
+    private static HashMap<Integer, List<Integer>> buildSet(int numNodes) {
+        HashMap<Integer, List<Integer>> set = new HashMap<Integer, List<Integer>>();
+        for(int i=1; i<=numNodes; i++){
+            List<Integer> list1 = new ArrayList<Integer>();
+            list1.add(i);
+            set.put(i, list1);
+        }
+        return set;
+    }
+    
+    private static void addEdge(HashMap<Integer, List<Edge>> graph, int source, int destination, int weight) {
+        if(graph.get(weight) == null ){ //there is no edge in the graph with the particular weight // create key
+            Edge e = new Edge(source, destination);
+            List<Edge> l = new ArrayList<Edge>();
+            l.add(e);
+            graph.put(weight, l);
+        }
+        else{ //already one edge with given weight exists, so append the list
+            Edge e = new Edge(source, destination);
+            List<Edge> l = graph.get(weight);
+            l.add(e);
+            graph.remove(weight);
+            graph.put(weight, l);
+        }
+    }
+
+    private static void printGraph(HashMap<Integer, List<Edge>> graph) {
+        System.out.println("");
+        for(Integer key:graph.keySet()){
+            List<Edge> l = graph.get(key);
+            System.out.print("Weight:"+key+"  ");
+            for(Edge e:l){
+                System.out.print(e.src+"-"+e.dst+"\t");
+            }System.out.println("");
+        }
+    }
+
+    private static void printSet(HashMap<Integer, List<Integer>> set) {
+        for(Integer key:set.keySet()){
+            List<Integer> list1 = set.get(key);
+            System.out.print("Key:"+key+"  ");
+            for(Integer i:list1){
+                System.out.print(i+" ");
+            }System.out.println("");
+        }
+    }
+
+
+    
+    
+    
+    
+    
+}
